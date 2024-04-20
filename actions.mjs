adda82913e54552555efce4bbd2f8922b6d0f271
Jeff Ardilio <jeff.ardilio@gmail.com>
2024-04-20 14:07:43 -0400

    fix for county tax

--- actions.mjs
+++ actions.mjs
@@ -271,7 +271,7 @@ function fixReservationMoney(r) {
 
     adjustments.grossWithTaxes = r.money.hostPayout - adjustments.hostChannelFees - adjustments.creditCardFees - adjustments.insuranceFees;
     adjustments.stateTaxes = adjustments.vat > 0 ? adjustments.grossWithTaxes * 0.065 : 0;
-    adjustments.countyTaxes = 0.050;
+    adjustments.countyTaxes = adjustments.grossWithTaxes * 0.050;
     adjustments.netIncome = adjustments.grossWithTaxes - adjustments.stateTaxes - adjustments.countyTaxes - adjustments.fareCleaning;
     adjustments.commission = adjustments.netIncome * 0.2;
     adjustments.ownerRevenue = adjustments.netIncome * 0.8;
